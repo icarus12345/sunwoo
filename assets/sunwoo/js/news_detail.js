ede1f05b1796cd7389d7f361155aac961a0f0da0
Truong <khuongxuantruong@gmail.com>
2019-02-18 22:57:25 +0700

    html

---
+++ assets/sunwoo/js/news_detail.js
@@ -0,0 +1,16 @@
+/*
+ * portfolio_detail.js
+ * ---
+ * @author takaaki.koyama
+ * ---
+ * ---------------------------------------------
+ * (c) baqemono.inc. all rights reserved.
+ * ---------------------------------------------
+ */
+(function(){
+
+
+	$(function(){
+		
+	});
+})(jQuery);
